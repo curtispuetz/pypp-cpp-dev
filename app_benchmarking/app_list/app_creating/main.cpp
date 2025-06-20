a83d79a765bd5d37eb5879308a577a227e1d1aa1
Curtis Puetz <36495179+curtispuetz@users.noreply.github.com>
2025-06-20 19:54:15 +0300

    more benchmarking and experimenting

--- app_benchmarking/app_list/app_creating/main.cpp
+++ app_benchmarking/app_list/app_creating/main.cpp
@@ -2,11 +2,17 @@
 #include "py_list.h"
 #include <cstdlib> // Required for EXIT_FAILURE
 #include <pypp_util/main_error_handler.h>
+
+// NOTE: I am actually thinking of not using std::move, and instead
+// if people want to achieve better performance, they can know that if they
+// inline the value to append to the list rather than using a variable that the
+// speed can be slightly improved.
 int main() {
     try {
         // Different ways to create a PyList
         // Results: The fastest way was found to create a PyList given a size
         // and the results were comparable to std::vector.
+        // It was also found when using std::move makes a speed difference.
         int list_size = 50000;
         int iter_length = 10;
         PyList<int> larger_list = PyList({1}) * list_size;
@@ -37,6 +43,13 @@ int main() {
                           list.append(std::move(inner));
                       }
                   });
+        benchmark("Same as above, but without move", [&]() {
+            PyList<PyList<int>> list;
+            for (int i = 0; i < iter_length; ++i) {
+                PyList<int> inner = PyList<int>({1}) * list_size;
+                list.append(inner);
+            }
+        });
         benchmark("create list of many creadted lists with move constructor, "
                   "different way",
                   [&]() {
@@ -49,14 +62,24 @@ int main() {
         benchmark("create list of many creadted lists with move constructor, "
                   "different second way",
                   [&]() {
-                      // This one is slower than above.
                       PyList<PyList<int>> list;
                       for (int i = 0; i < iter_length; ++i) {
                           PyList<int> inner = PyList<int>({1});
                           inner *= list_size;
-                          list.append(std::move(PyList<int>({1}) * list_size));
+                          list.append(std::move(inner));
                       }
                   });
+        benchmark("Same as above, but without move", [&]() {
+            // Without the move makes a difference here! Because the variable
+            // exists. I think if the variable does not exist and is inlined,
+            // then it doesn't matter if you use std::move or not.
+            PyList<PyList<int>> list;
+            for (int i = 0; i < iter_length; ++i) {
+                PyList<int> inner = PyList<int>({1});
+                inner *= list_size;
+                list.append(inner);
+            }
+        });
         benchmark("create list of many creadted lists with move constructor, "
                   "different new third way",
                   [&]() {
@@ -68,6 +91,14 @@ int main() {
                               std::move(create_list_full(list_size, 1)));
                       }
                   });
+        benchmark("same as above, but without move", [&]() {
+            // This is equivalent to the speed of the std::vector
+            // version below.
+            PyList<PyList<int>> list;
+            for (int i = 0; i < iter_length; ++i) {
+                list.append(create_list_full(list_size, 1));
+            }
+        });
         benchmark("std::vector equavlanet to above", [&]() {
             std::vector<std::vector<int>> list;
             list.reserve(iter_length);
---
+++ app_benchmarking/app_set/main.cpp
@@ -0,0 +1,68 @@
+#include "benchmark.h"
+#include "py_set.h"
+#include <cstdlib> // Required for EXIT_FAILURE
+#include <pypp_util/main_error_handler.h>
+#include <string>
+
+int main() {
+    try {
+        // Test the speed of differeent ways to initialize a PySet.
+        // Results: I think the results are comparable. It depends on which you
+        // put first. The first one is typically slower. The average over the
+        // 500000 runs is equal.
+        // Conclusion: The common way is fine.
+        benchmark(
+            "PySet optimal", [&]() { PySet<int> py_set({1, 2, 3}); }, 50000);
+        benchmark(
+            "PySet Common", [&]() { PySet<int> py_set = PySet({1, 2, 3}); },
+            50000);
+
+        // Test Add speed
+        // Results: The speed is identical to std::unordered_set insert.
+        const int size = 50000;
+        benchmark("PySet add", [&]() {
+            PySet<int> py_set;
+            for (int i = 0; i < size; ++i) {
+                py_set.add(i);
+            }
+        });
+        benchmark("std::unordered_set insert (i.e. add)", [&]() {
+            std::unordered_set<int> std_set;
+            for (int i = 0; i < size; ++i) {
+                std_set.insert(i);
+            }
+        });
+
+        // Test creation speed with big elements vs. std::unordered_set.
+        // Results: same speed as std::unordered_set.
+        // Conclusion: I am thinking about just not using the std::move
+        // and worry about that latter. Because I think for my programs
+        // I am going to be fine without it.
+        benchmark("PySet with big elements", [&]() {
+            PySet<std::string> py_set;
+            for (int i = 0; i < 10; ++i) {
+                std::string big_string(10000, 'a');
+                py_set.add(big_string);
+            }
+        });
+        benchmark("std::unordered_set with big elements", [&]() {
+            std::unordered_set<std::string> std_set;
+            for (int i = 0; i < 10; ++i) {
+                std::string big_string(10000, 'a');
+                std_set.insert(big_string);
+            }
+        });
+        benchmark("std::unordered_set with big elements and std::move", [&]() {
+            std::unordered_set<std::string> std_set;
+            for (int i = 0; i < 10; ++i) {
+                std::string big_string(10000, 'a');
+                std_set.insert(std::move(big_string));
+            }
+        });
+
+        return 0;
+    } catch (...) {
+        handle_fatal_exception();
+        return EXIT_FAILURE;
+    }
+}
