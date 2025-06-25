16279191b97f66a94c41b5177358fe9bcdc2ac50
Curtis Puetz <puetzcurtis@gmail.com>
2025-06-25 12:06:54 +0300

    test 4D array creation with np::array

--- app_random_tests/app_numpy_arrays/main.cpp
+++ app_random_tests/app_numpy_arrays/main.cpp
@@ -118,6 +118,15 @@ int main() {
                 {10, 11, 12}, {13, 14, 15}, {16, 17, 18}}};
         NpArr<int> arr3D2 = np::array<int>(std::move(data3d));
         print(arr3D2.to_string());
+        print("creating 4D array with np::array: ");
+        std::vector data4d = {std::vector<std::vector<std::vector<int>>>{
+                                  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}},
+                                  {{10, 11, 12}, {13, 14, 15}, {16, 17, 18}}},
+                              std::vector<std::vector<std::vector<int>>>{
+                                  {{19, 20, 21}, {22, 23, 24}, {25, 26, 27}},
+                                  {{28, 29, 30}, {31, 32, 33}, {34, 35, 36}}}};
+        NpArr<int> arr4D = np::array<int>(std::move(data4d));
+        print(arr4D.to_string());
     } catch (...) {
         handle_fatal_exception();
         return EXIT_FAILURE;
