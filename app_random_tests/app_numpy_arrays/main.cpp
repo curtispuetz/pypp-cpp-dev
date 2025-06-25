f4c2e0e8d9ec142377e9c652733d1e854a9261ca
Curtis Puetz <puetzcurtis@gmail.com>
2025-06-25 12:04:37 +0300

    fix the np::array method

--- app_random_tests/app_numpy_arrays/main.cpp
+++ app_random_tests/app_numpy_arrays/main.cpp
@@ -103,14 +103,21 @@ int main() {
         // test np::array creation function
         print("creating 1D array with np::array: ");
         std::vector<int> data = {1, 2, 3, 4, 5};
-        NpArr<int> arr1D = np::array(std::move(data));
+        NpArr<int> arr1D = np::array<int>(std::move(data));
         print(arr1D.to_string());
         std::vector data2d = {std::vector<int>{1, 2, 3},
                               std::vector<int>{4, 5, 6},
                               std::vector<int>{7, 8, 9}};
         print("creating 2D array with np::array: ");
-        NpArr<int> arr2D = np::array(std::move(data2d));
+        NpArr<int> arr2D = np::array<int>(std::move(data2d));
         print(arr2D.to_string());
+        print("creating 3D array with np::array: ");
+        std::vector data3d = {
+            std::vector<std::vector<int>>{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}},
+            std::vector<std::vector<int>>{
+                {10, 11, 12}, {13, 14, 15}, {16, 17, 18}}};
+        NpArr<int> arr3D2 = np::array<int>(std::move(data3d));
+        print(arr3D2.to_string());
     } catch (...) {
         handle_fatal_exception();
         return EXIT_FAILURE;
