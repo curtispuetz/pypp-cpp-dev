a39e45826bb97aeef61a9f82b3d911810505a879
Curtis Puetz <36495179+curtispuetz@users.noreply.github.com>
2025-06-25 12:04:37 +0300

    fix the np::array method

--- pypp/numpy/creators/arr.h
+++ pypp/numpy/creators/arr.h
@@ -1,100 +1,74 @@
 #pragma once
+
 #include "numpy/np_arr.h"
 #include "py_list.h"
+#include <cstddef>
 #include <stdexcept>
 #include <type_traits>
-#include <variant>
 #include <vector>
 
-// Throw if ragged array (inconsistent subvector sizes)
-template <typename T>
-void throw_if_ragged_recursive(const std::vector<T> &data) {
-    // Base case: not a vector of vectors, nothing to check
-}
+// Helper to check if a type is a vector
+template <typename T> struct is_vector : std::false_type {};
+template <typename T, typename Alloc>
+struct is_vector<std::vector<T, Alloc>> : std::true_type {};
 
-template <typename T>
-void throw_if_ragged_recursive(const std::vector<std::vector<T>> &data) {
+template <typename U>
+void _throw_if_ragged_recursive(const std::vector<U> &data) {
     if (data.empty())
         return;
-    size_t first_len = data[0].size();
-    for (const auto &sublist : data) {
-        if (sublist.size() != first_len) {
-            throw std::invalid_argument(
-                "Inconsistent list lengths found when initializing numpy "
-                "array; array is ragged.");
+    if constexpr (is_vector<U>::value) {
+        size_t first_len = data[0].size();
+        for (const auto &sublist : data) {
+            if (!is_vector<U>::value || sublist.size() != first_len) {
+                throw std::invalid_argument(
+                    "Inconsistent list lengths found when initializing numpy "
+                    "array; array is ragged.");
+            }
+            _throw_if_ragged_recursive(sublist);
         }
-        throw_if_ragged_recursive(sublist);
     }
 }
 
-// Deduce shape
-template <typename T>
-void deduce_shape_recursive(const std::vector<T> &data, PyList<int> &shape) {
+template <typename U>
+void _deduce_shape_recursive(const std::vector<U> &data, PyList<int> &shape) {
     shape.append(static_cast<int>(data.size()));
-}
-
-template <typename T>
-void deduce_shape_recursive(const std::vector<std::vector<T>> &data,
-                            PyList<int> &shape) {
-    shape.append(static_cast<int>(data.size()));
-    if (!data.empty()) {
-        deduce_shape_recursive(data[0], shape);
+    if constexpr (is_vector<U>::value) {
+        _deduce_shape_recursive(data[0], shape);
     }
 }
 
-template <typename T> PyList<int> deduce_shape(const std::vector<T> &data) {
+template <typename U> PyList<int> _deduce_shape(const std::vector<U> &data) {
     PyList<int> shape;
-    deduce_shape_recursive(data, shape);
+    _deduce_shape_recursive(data, shape);
     return shape;
 }
 
-// Flatten
-template <typename T>
-void flatten_recursive(const std::vector<T> &data, std::vector<T> &flattened) {
-    flattened.insert(flattened.end(), data.begin(), data.end());
-}
-
-template <typename T>
-void flatten_recursive(const std::vector<std::vector<T>> &data,
-                       std::vector<T> &flattened) {
-    for (const auto &sublist : data) {
-        flatten_recursive(sublist, flattened);
+template <typename T, typename U>
+void _flatten_recursive(const std::vector<U> &data, std::vector<T> &flattened) {
+    if constexpr (is_vector<U>::value) {
+        for (const auto &sublist : data) {
+            _flatten_recursive<T>(sublist, flattened);
+        }
+    } else {
+        flattened.insert(flattened.end(), data.begin(), data.end());
     }
 }
 
-template <typename T> std::vector<T> flatten(const std::vector<T> &data) {
+template <typename T, typename U>
+std::vector<T> _flatten(const std::vector<U> &data) {
     std::vector<T> flattened;
-    flatten_recursive(data, flattened);
+    _flatten_recursive<T>(data, flattened);
     return flattened;
 }
 
-template <typename T>
-std::vector<T> flatten(const std::vector<std::vector<T>> &data) {
-    std::vector<T> flattened;
-    flatten_recursive(data, flattened);
-    return flattened;
-}
-// TODO: there is still something not right here.
 namespace np {
-// Main np_array function
-template <typename T> NpArr<T> array(const std::vector<T> data) {
+template <typename T, typename U> NpArr<T> array(const std::vector<U> data) {
     if (data.empty()) {
         throw std::invalid_argument("empty numpy array is not supported");
     }
-    throw_if_ragged_recursive(data);
-    auto shape = deduce_shape(data);
-    auto flat_data = flatten(data);
-    return NpArr<T>::create(std::move(shape), std::move(flat_data));
-}
-
-template <typename T> NpArr<T> array(const std::vector<std::vector<T>> data) {
-    // TODO: make this accept a PyList
-    if (data.empty()) {
-        throw std::invalid_argument("empty numpy array is not supported");
-    }
-    throw_if_ragged_recursive(data);
-    auto shape = deduce_shape(data);
-    auto flat_data = flatten(data);
+    _throw_if_ragged_recursive(data);
+    auto shape = _deduce_shape(data);
+    auto flat_data = _flatten<T>(data);
     return NpArr<T>::create(std::move(shape), std::move(flat_data));
 }
 } // namespace np
