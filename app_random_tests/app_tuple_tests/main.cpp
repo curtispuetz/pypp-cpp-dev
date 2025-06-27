37cc0735db70953e7bc349e22116617edb0f7938
Curtis Puetz <36495179+curtispuetz@users.noreply.github.com>
2025-06-27 11:45:04 +0300

    add tests for tuple unpacking

---
+++ app_random_tests/app_tuple_tests/main.cpp
@@ -0,0 +1,33 @@
+#include "py_str.h"
+#include "py_tuple.h"
+#include <cstdlib> // Required for EXIT_FAILURE
+#include <iostream>
+#include <pypp_util/main_error_handler.h>
+
+PyTup<int, double, PyStr> get_tuple() {
+    return PyTup<int, double, PyStr>(100, 2.718, PyStr("World"));
+}
+
+int main() {
+    try {
+        // getting elements from a tuple
+        PyTup<int, double, PyStr> tup = PyTup(42, 3.14, PyStr("Hello"));
+        const auto &[a, b, c] = tup;
+        std::cout << "Tuple elements:\n";
+        std::cout << "a: " << a << "\n";
+        std::cout << "b: " << b << "\n";
+        std::cout << "c: " << c.str() << "\n";
+
+        // getting elements from a tuple returned by a function
+        const auto &[x, y, z] = get_tuple();
+        std::cout << "Tuple from function:\n";
+        std::cout << "x: " << x << "\n";
+        std::cout << "y: " << y << "\n";
+        std::cout << "z: " << z.str() << "\n";
+
+        return 0;
+    } catch (...) {
+        handle_fatal_exception();
+        return EXIT_FAILURE;
+    }
+}
