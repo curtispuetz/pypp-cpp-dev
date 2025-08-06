42c07cbed6cfffd73099620690dabc3cb9f4a987
Curtis Puetz <36495179+curtispuetz@users.noreply.github.com>
2025-08-06 08:09:18 -0700

    test

--- app_random_tests/app_time/main.cpp
+++ app_random_tests/app_time/main.cpp
@@ -7,12 +7,12 @@ int main() {
     try {
 
         auto time_start = pypp_time::start();
-        pypp_time::sleep(0.1);
+        pypp_time::sleep(0.01); // won't sleeps exactly this short.
         auto elapsed = pypp_time::end(time_start);
         std::cout << "Elapsed time: " << elapsed << " seconds" << std::endl;
 
         auto perf_start = pypp_time::perf_counter_start();
-        pypp_time::sleep(0.1);
+        pypp_time::sleep(0.01);
         auto perf_elapsed = pypp_time::perf_counter_end(perf_start);
         std::cout << "High-resolution elapsed time: " << perf_elapsed
                   << " seconds" << std::endl;
