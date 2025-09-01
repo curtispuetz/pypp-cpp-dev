61539d415d540dd5d1bdb783b214dd9d132aa7e0
Curtis Puetz <puetzcurtis@gmail.com>
2025-09-01 10:46:37 -0700

    add some dict tests

--- app_random_tests/app_dict/main.cpp
+++ app_random_tests/app_dict/main.cpp
@@ -19,6 +19,12 @@ int main() {
         for (const auto &item : int_dict.items()) {
             print("item:", item.get<0>(), "->", item.get<1>());
         }
+        int_dict[7] = PyStr("d");
+        print("After adding 7->d:", int_dict);
+        int a = 3;
+        int_dict[std::move(a)] = PyStr("new_value");
+        int_dict.dg(3);
+        int_dict.dg(a);
     } catch (...) {
         handle_fatal_exception();
         return EXIT_FAILURE;
