9a8dc9b6b344aea580ac4e0e48e321e755f6b3c3
Curtis Puetz <puetzcurtis@gmail.com>
2025-09-07 10:42:16 -0700

    fix issue with pypp_get_resources

--- pypp/pypp_resources.cpp
+++ pypp/pypp_resources.cpp
@@ -1,9 +1,11 @@
 #include "pypp_resources.h"
+#include "pypp_os.h"
+#include "pypp_platform.h"
 
 namespace pypp {
 PyStr pypp_get_resources(const PyStr &relative_path) {
     PyStr exe_dir = platform::get_executable_dir();
-    return os::path::join(exe_dir, PyStr(".."), PyStr(".."), PyStr(".."),
-                          PyStr("resources"), relative_path);
+    return os::path::join(exe_dir, PyStr(".."), PyStr(".."), PyStr("resources"),
+                          relative_path);
 }
 } // namespace pypp
