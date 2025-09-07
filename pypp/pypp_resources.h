3518c628a9f086d3ebf1e4e343a7b60537338adb
Curtis Puetz <36495179+curtispuetz@users.noreply.github.com>
2025-09-07 10:42:16 -0700

    fix issue with pypp_get_resources

--- pypp/pypp_resources.h
+++ pypp/pypp_resources.h
@@ -1,9 +1,6 @@
 #pragma once
 
-#include "exceptions/stdexcept.h"
 #include "py_str.h"
-#include "pypp_os.h"
-#include "pypp_platform.h"
 
 namespace pypp {
 PyStr pypp_get_resources(const PyStr &relative_path);
