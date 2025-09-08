3db57656eb0e76d0d12b63f0e734f4b06929f825
Curtis Puetz <36495179+curtispuetz@users.noreply.github.com>
2025-09-08 16:33:34 -0700

    support default dict for list, set, and dict functions

--- pypp/pypp_util/create/dict.h
+++ pypp/pypp_util/create/dict.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "py_dict.h"
+#include "py_dict_default.h"
 #include "py_list.h"
 #include "py_tuple.h"
 
@@ -19,6 +20,11 @@ PyDict<T, U> dict(const PyList<PyTup<T, U>> &lst_of_tuples) {
             std::move(tup.template get<1>());
     }
     return dict;
+}
+
+template <typename T, typename U>
+PyDict<T, U> dict(const PyDefaultDict<T, U> &dict) {
+    return PyDict<T, U>(dict.data.data.begin(), dict.data.data.end());
 }
 
 template <typename T, typename U> PyDict<T, U> dict() { return PyDict<T, U>(); }
