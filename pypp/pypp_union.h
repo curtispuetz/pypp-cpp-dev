f2de98f7a6824cc201a825b326da50f7ffeac4ac
Curtis Puetz <36495179+curtispuetz@users.noreply.github.com>
2025-09-09 11:12:02 -0700

    support printing and comparison operators for Uni

--- pypp/pypp_union.h
+++ pypp/pypp_union.h
@@ -1,4 +1,7 @@
 #pragma once
+
+#include <iostream>
+#include <sstream>
 #include <type_traits>
 #include <variant>
 
@@ -30,10 +33,29 @@ template <typename... Types> class Uni {
     // Get value as T (throws if wrong type)
     template <typename T> T &ug() { return std::get<T>(data_); }
 
+    bool operator==(const Uni &other) const { return data_ == other.data_; }
+    bool operator!=(const Uni &other) const { return !(*this == other); }
+
+    void print(std::ostream &os) const {
+        std::visit([&os](const auto &value) { os << "Union[" << value << "]"; },
+                   data_);
+    }
+
+    void print() const {
+        print(std::cout);
+        std::cout << std::endl;
+    }
+
   private:
     VariantType data_;
 };
 
-// dedction guide
+template <typename... Types>
+std::ostream &operator<<(std::ostream &os, const pypp::Uni<Types...> &u) {
+    u.print(os);
+    return os;
+}
+
+// deduction guide
 template <typename... Ts> Uni(Ts...) -> Uni<Ts...>;
 } // namespace pypp
