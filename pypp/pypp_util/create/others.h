2421b27e1691b791d4f30d4a9be86d7c24da63fb
Curtis Puetz <36495179+curtispuetz@users.noreply.github.com>
2025-09-12 15:02:09 -0700

    add type conversion support

---
+++ pypp/pypp_util/create/others.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <iostream>
+
+namespace pypp {
+
+template <typename T> inline int int_(const T &value) {
+    return static_cast<int>(value);
+}
+
+inline int int_(const PyStr &value) { return std::stoi(value.str()); }
+
+template <typename T> inline double float_(const T &value) {
+    return static_cast<double>(value);
+}
+
+inline double float_(const PyStr &value) { return std::stod(value.str()); }
+
+template <typename T> inline bool bool_(const T &value) {
+    return static_cast<bool>(value);
+}
+
+inline bool bool_(const PyStr &value) { return !value.str().empty(); }
+
+template <typename T> inline float to_float32(const T &value) {
+    return static_cast<float>(value);
+}
+
+inline float to_float32(const PyStr &value) { return std::stof(value.str()); }
+
+} // namespace pypp
