a3879d440089bb46ab1768f41cba3df24adeafe3
Curtis Puetz <puetzcurtis@gmail.com>
2025-09-13 12:34:32 -0700

    throw exceptions when string conversion to numbers does not work

--- pypp/pypp_util/create/others.h
+++ pypp/pypp_util/create/others.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "exceptions/common.h"
 #include "py_str.h"
 #include <iostream>
 
@@ -9,13 +10,27 @@ template <typename T> inline int int_(const T &value) {
     return static_cast<int>(value);
 }
 
-inline int int_(const PyStr &value) { return std::stoi(value.str()); }
+inline int int_(const PyStr &value) {
+    try {
+        return std::stoi(value.str());
+    } catch (...) {
+        throw ValueError(PyStr("Invalid literal for int() with base 10: '") +
+                         value + PyStr("'"));
+    }
+}
 
 template <typename T> inline double float_(const T &value) {
     return static_cast<double>(value);
 }
 
-inline double float_(const PyStr &value) { return std::stod(value.str()); }
+inline double float_(const PyStr &value) {
+    try {
+        return std::stod(value.str());
+    } catch (...) {
+        throw ValueError(PyStr("Could not convert string to float: '") + value +
+                         PyStr("'"));
+    }
+}
 
 template <typename T> inline bool bool_(const T &value) {
     return static_cast<bool>(value);
@@ -27,6 +42,13 @@ template <typename T> inline float to_float32(const T &value) {
     return static_cast<float>(value);
 }
 
-inline float to_float32(const PyStr &value) { return std::stof(value.str()); }
+inline float to_float32(const PyStr &value) {
+    try {
+        return std::stof(value.str());
+    } catch (...) {
+        throw ValueError(PyStr("Could not convert string to float32: '") +
+                         value + PyStr("'"));
+    }
+}
 
 } // namespace pypp
